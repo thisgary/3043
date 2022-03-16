2800032a6434eb8be0f8147f2530832c0b6a40dd
Gary <gary.github@gmail.com>
2022-03-16 18:58:34 +0800

    No reason to exist anymore

--- src/main/java/com/thisgary/library/Dumpster.java
+++ src/main/java/com/thisgary/library/Dumpster.java
@@ -1,11 +1,6 @@
 package com.thisgary.library;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.swing.JOptionPane;
 
 public class Dumpster {
     public static int dayInMonth(int m, int y) {
@@ -29,40 +24,6 @@ public class Dumpster {
     public static void PSVInvoker(String packageClassName, String methodName) throws Throwable {
         Method m = Class.forName(packageClassName).getMethod(methodName, (Class<?>[]) null);
         m.invoke(null, (Object[]) null);
-    }
-
-    // it exists
-    public static void moduleActivity(int i) throws Throwable {
-        // Get list of options in module i
-        final Class<?> CLASS = Class.forName("com.thisgary.lab.module" + i + ".Module" + i);
-        final Method[] METHODS = CLASS.getDeclaredMethods();
-
-        // List options in module i
-        final Pattern P = Pattern.compile("activity[0-9]+");
-        List<String> options = new ArrayList<>();
-        for (Method method : METHODS) {
-            Matcher m = P.matcher(method.getName());
-            if (m.matches()) {
-                String name = m.group(0);
-                options.add(name.substring(name.length() - 1));
-            }
-        }
-        final String REF = "Activities: " + String.join(", ", options);
-
-        while (true) {
-            int choice = GetNumber.jopInt(REF, "Lab Module " + i);
-            if (options.contains(String.valueOf(choice))) {
-                Method activity = CLASS.getMethod("activity" + choice, (Class<?>[]) null);
-                activity.invoke(null, (Object[]) null);
-            } else {
-                JOptionPane.showMessageDialog(
-                        null,
-                        "activity" + choice + "() does not exist in Module" + i,
-                        "Error",
-                        JOptionPane.ERROR_MESSAGE
-                );
-            }
-        }
     }
 
     public static String repeat(int length, String that) {
